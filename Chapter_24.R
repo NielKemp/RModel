cc3a5c71cc514affc0c25fc40449066f69a82a89
NielKemp <nielqi@gmail.com>
2019-05-26 21:06:47 +0200

    Removed redundent functions and added model predictions

--- Chapter_24.R
+++ Chapter_24.R
@@ -40,52 +40,6 @@ ggplot(diamonds2, aes(lcarat, lprice))+geom_hex(bins = 50)
 mod_diamond = lm(lprice~lcarat, data = diamonds2) #fits simple linear model (yvar = xvar)
 
 
-#need to fix some functions because reasons...
-#first fixed function
-data_grid2 <- function (data, ..., .model = NULL) {
-  expanded <- tidyr::expand(data, ...)
-  if (is.null(.model)) 
-    return(expanded)
-  needed <- setdiff(modelr:::predictor_vars(.model), names(expanded))
-  typical <- tidyr::crossing(!!! lapply(data[needed], typical))
-  tidyr::crossing(expanded, typical)
-}
-
-
-#second fixed function
-
-seq_range <- function(x, n, by, trim = NULL, expand = NULL, pretty = FALSE) {
-  if (!missing(n) && !missing(by)) {
-    stop("May only specify one of `n` and `by`", call. = FALSE)
-  }
-  
-  if (!is.null(trim)) {
-    rng <- stats::quantile(x, c(trim / 2, 1 - trim / 2), na.rm = TRUE)
-  } else {
-    rng <- range(x, na.rm = TRUE)
-  }
-  
-  if (!is.null(expand)) {
-    rng <- rng + c(-expand / 2, expand / 2) * (rng[2] - rng[1])
-  }
-  
-  if (missing(by)) {
-    if (pretty) {
-      pretty(rng, n)
-    } else {
-      seq(rng[1], rng[2], length.out = n)
-    }
-  } else {
-    if (pretty) {
-      rng[1] <- floor(rng[1] / by) * by
-      rng[2] <- ceiling(rng[2] / by) * by
-    }
-    seq(rng[1], rng[2], by = by)
-  }
-}
-
-#third fixed function
-
 #add predictions back to data, need to remove the log effect
-grid <- diamonds2 %>% data_grid2(carat = seq_range(carat,n=20))%>%mutate (lcarat = log2(carat)) %>% add_predictions(mod_diamond, "lprice")%>% mutate(price = 2^lprice)
+grid <- diamonds2 %>% data_grid(carat = seq_range(carat,n=20))%>%mutate (lcarat = log2(carat)) %>% add_predictions(mod_diamond, "lprice")%>% mutate(price = 2^lprice)
 
